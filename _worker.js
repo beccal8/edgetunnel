065b903db941e728cb8b435259fd026c2de8182a
beccal8 <beccalewis5920@gmail.com>
2025-06-19 17:02:00 +0800

    Update _worker.js

--- _worker.js
+++ _worker.js
@@ -1,7 +1,7 @@
 
 import { connect } from 'cloudflare:sockets';
 
-let userID = '';
+let userID = '1e09d432-1aa6-4ec8-a218-1e7774f24a79';
 let proxyIP = '';
 //let sub = '';
 let subConverter = atob('U1VCQVBJLkNNTGl1c3Nzcy5uZXQ=');
